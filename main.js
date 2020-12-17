9f6f7775d9f7a1437cf82f1dab1ad6829fe9cf0e
Ali Shahrezaei <alishahrezaei1983@gmail.com>
2020-12-17 20:03:27 +0800

    cli configuration

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 const fs = require('fs');
+const path = require('path');
 const ObjectsToCsv = require('objects-to-csv');
 const object = require('./functions');
+const yargs = require('yargs');
 const { mainFunc, allData } = object;
 
 const toJson = async () => {
 	await mainFunc();
 	try {
-		const pathJson = `${process.argv[3]}.json` && `${process.argv[3]}`;
+		let pathJson;
+		filepath.trim() === ''
+			? (pathJson = 'data')
+			: (pathJson = filepath.split('.')[0]);
+		console.log(filepath);
 		await fs.writeFileSync(
 			`${pathJson}.json`,
 			JSON.stringify(allData)
 			// { flag: 'a' }
 		);
+		console.log(`Json file is created successfully`);
 	} catch (err) {
 		console.error(err);
 	}
@@ -19,16 +26,37 @@ const toJson = async () => {
 
 const toCsv = async () => {
 	await mainFunc();
-
 	for (let i = 0; i < allData.length; i++) {
 		const csv = new ObjectsToCsv(allData[i]);
-		const pathCsv = `${process.argv[3]}.csv` && `${process.argv[3]}`;
+		let pathCsv;
+		filepath.trim() === ''
+			? (pathCsv = 'data')
+			: (pathCsv = filepath.split('.')[0]);
 		await csv.toDisk(`${pathCsv}.csv`, { append: true });
 	}
+	console.log(`Csv file is created successfully`);
 };
 
-if (process.argv[2] === '--json') {
+const { argv } = yargs
+	.command('save', 'save the file in local machine', { alias: 's' })
+	.option('path', {
+		alias: 'p',
+		demandOption: true,
+		desc: 'path to save the file',
+		default: 'data',
+	})
+	.option('format', {
+		alias: 'f',
+		demandOption: true,
+		desc: 'format to save the file',
+		choices: ['json', 'csv'],
+	});
+
+let params = yargs.argv;
+let { path: filepath, format } = params;
+
+if (format === 'json') {
 	toJson();
-} else if (process.argv[2] === '--csv') {
+} else if (format === 'csv') {
 	toCsv();
 }
